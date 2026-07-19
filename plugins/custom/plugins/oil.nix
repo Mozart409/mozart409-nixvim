a1d346a029bb7644a2a5b1047c1027ba5644a25b
Amadeus Mader <amadeus@mozart409.com>
2026-07-19 13:22:29 +0200

    feat(oil): configure oil

--- plugins/custom/plugins/oil.nix
+++ plugins/custom/plugins/oil.nix
@@ -2,21 +2,11 @@
   programs.nixvim = {
     plugins = {
       oil = {
-        enable = false;
+        enable = true;
         settings = {
           delete_to_trash = true;
           skip_confirm_for_simple_edits = true;
-          use_default_keymaps = false;
-          keymaps = {
-            "g?" = "actions.show_help";
-            "<CR>" = "actions.select";
-            "-" = "actions.parent";
-            "_" = "actions.open_cwd";
-            "`" = "actions.cd";
-            "~" = "actions.tcd";
-            "g." = "actions.toggle_hidden";
-          };
-
+          use_default_keymaps = true;
           view_options = {
             show_hidden = true;
           };
