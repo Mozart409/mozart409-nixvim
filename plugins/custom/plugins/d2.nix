03a1e646bf3aa992639f11876c8dc202bf72ce58
Amadeus Mader <amadeus@mozart409.com>
2026-08-03 01:00:12 +0200

    fix(d2): remove default leader mappings, keep syntax only

--- plugins/custom/plugins/d2.nix
+++ plugins/custom/plugins/d2.nix
@@ -15,6 +15,28 @@
           end
         '';
       }
+
+      # d2-vim's plugin/d2.vim unconditionally claims <Leader>d2, <Leader>rd2
+      # and <Leader>yd2 globally (no opt-out global to set). We only want the
+      # syntax support, and those maps squat inside the which-key [D]ocument
+      # and [R]ename groups with no description. Drop them once the plugin's
+      # own plugin/ scripts have been sourced -- the commands (:D2Preview*,
+      # :D2ReplaceSelection) stay available.
+      {
+        event = ["VimEnter"];
+        desc = "Remove d2-vim's default <Leader> mappings";
+        callback.__raw = ''
+          function()
+            -- Deferred to the end of startup so it lands after plugin/d2.vim
+            -- has been sourced, whatever the load order works out to be.
+            vim.schedule(function()
+              pcall(vim.keymap.del, "v", "<Leader>d2")
+              pcall(vim.keymap.del, "v", "<Leader>rd2")
+              pcall(vim.keymap.del, "n", "<Leader>yd2")
+            end)
+          end
+        '';
+      }
     ];
   };
 }
