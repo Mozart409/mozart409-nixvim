73fedf5d213f432737792c4bea2347d466f8fece
Amadeus Mader <amadeus@mozart409.com>
2026-09-15 22:02:53 +0200

    chore: simplify kickstart plugins

--- plugins/kickstart/plugins/indent-blankline.nix
+++ plugins/kickstart/plugins/indent-blankline.nix
@@ -1,10 +1,3 @@
 {
-  programs.nixvim = {
-    # Add indentation guides even on blank lines
-    # For configuration see `:help ibl`
-    # https://nix-community.github.io/nixvim/plugins/indent-blankline/index.html
-    plugins.indent-blankline = {
-      enable = true;
-    };
-  };
+  programs.nixvim.plugins.indent-blankline.enable = true;
 }
--- plugins/kickstart/plugins/lint.nix
+++ plugins/kickstart/plugins/lint.nix
@@ -1,28 +1,10 @@
-{ ...}: {
+_: {
   programs.nixvim = {
-    # Linting
-    # https://nix-community.github.io/nixvim/plugins/lint/index.html
     plugins.lint = {
       enable = true;
-
-      # NOTE: Enabling these will cause errors unless these tools are installed
       lintersByFt = {
         nix = ["nix"];
-        markdown = [
-          # "vale"
-        ];
-        #clojure = ["clj-kondo"];
-        #inko = ["inko"];
-        #janet = ["janet"];
-        # json = [ "jsonlint" ];
-        #rst = ["vale"];
-        #ruby = ["ruby"];
-        #terraform = ["tflint"];
-        # text = ["vale"];  # Disabled: vale exits with code 2 without .vale.ini config
       };
-
-      # Create autocommand which carries out the actual linting
-      # on the specified events.
       autoCmd = {
         callback.__raw = ''
           function()
@@ -31,20 +13,12 @@
         '';
         group = "lint";
         event = [
-          # keep-sorted start
           "BufEnter"
           "BufWritePost"
           "InsertLeave"
-          # keep-sorted end
         ];
       };
     };
-
-    # https://nix-community.github.io/nixvim/NeovimOptions/autoGroups/index.html
-    autoGroups = {
-      lint = {
-        clear = true;
-      };
-    };
+    autoGroups.lint.clear = true;
   };
 }
